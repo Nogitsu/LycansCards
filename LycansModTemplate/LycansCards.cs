b08d7f67fe0fbd2eaff3004025d876f9d123a606
Nogitsu <nogitsu.9@gmail.com>
2024-07-24 18:35:37 +0200

    Renamed from template

---
+++ LycansModTemplate/LycansCards.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace LycansModTemplate
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("Lycans.exe")]
+    public class LycansCards : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = $"nm-qm.lycans-cards";
+        public const string PLUGIN_AUTHOR = "NM & QM";
+        public const string PLUGIN_NAME = "Lycans Cards";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            Log.Init(Logger);
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
