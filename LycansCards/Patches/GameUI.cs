2af17392a62d83344864e8127dff8e5a6eee69b6
Nogitsu <nogitsu.9@gmail.com>
2024-07-25 11:38:05 +0200

    Removed version wrap + Hide player's role and color as spectator

--- LycansCards/Patches/GameUI.cs
+++ LycansCards/Patches/GameUI.cs
@@ -8,6 +8,8 @@ namespace LycansCards.Patches
         public static void Patch()
         {
             On.GameUI.ShowMainMenu += OnShowMainMenu;
+            On.GameUI.ShowRole += OnShowRole;
+            On.GameUI.ShowSpectateMenu += OnShowSpectateMenu;
         }
 
         private static void OnShowMainMenu(On.GameUI.orig_ShowMainMenu orig, global::GameUI self, bool active)
@@ -15,17 +17,27 @@ namespace LycansCards.Patches
             int modsCount = LycansCards.CountMods();
 
             GameObject versionObj = GameObject.Find("/GameUI/Canvas/MainMenu/LayoutGroup/Footer/Version");
-            if (versionObj != null)
-            {
-                TextMeshProUGUI tmPro = versionObj.GetComponent<TextMeshProUGUI>();
-                if (tmPro != null)
-                {
-                    string mods = modsCount > 1 ? "mods" : "mod";
-                    tmPro.SetText("Modded version " + Application.version.ToString() + " (" + modsCount + " " + mods + " loaded)");
-                }
-            }
+            TextMeshProUGUI tmPro = versionObj.GetComponent<TextMeshProUGUI>();
+            
+            string mods = modsCount > 1 ? "mods" : "mod";
+            tmPro.SetText("Modded version " + Application.version.ToString() + " (" + modsCount + " " + mods + " loaded)");
+            tmPro.enableWordWrapping = false;
 
             orig(self, active);
+        }
+
+        private static void OnShowRole(On.GameUI.orig_ShowRole orig, global::GameUI self, bool active)
+        {
+            orig(self, active);
+            GameObject.Find("GameUI/Canvas/Game/Role").SetActive(false);
+        }
+
+        private static void OnShowSpectateMenu(On.GameUI.orig_ShowSpectateMenu orig, global::GameUI self, bool active)
+        {
+            orig(self, active);
+            GameObject spectateInfo = GameObject.Find("GameUI/Canvas/Spectate/SpectateInfo");
+            TextMeshProUGUI tmPro = spectateInfo.GetComponent<TextMeshProUGUI>();
+            tmPro.color = new Color(1f, 1f, 1f, 0.8f);
         }
     }
 }
