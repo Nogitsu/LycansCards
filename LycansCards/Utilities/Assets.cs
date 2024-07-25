94908d545ee9a24585598e6d36bcd4e9104f7bbf
QuenK30 <quentinmonin30310@gmail.com>
2024-07-25 13:27:25 +0200

    Refacto cards + assets

--- LycansCards/Utilities/Assets.cs
+++ LycansCards/Utilities/Assets.cs
@@ -10,8 +10,16 @@ namespace LycansCards.Utilities
 {
     public class Assets
     {
+
+        //const cards
+        private const string VILLAGER_CARD = "villager_card.png";
+        private const string HUNTER_CARD = "hunter_card.png";
+        private const string ALCHEMIST_CARD = "alchemist_card.png";
+        private const string WEREWOLF_CARD = "werewolf_card.png";
+        private const string CARD_BACK = "card_back.png";
+
         static private readonly string assetsPath = Paths.PluginPath + "/" + LycansCards.PLUGIN_FOLDER + "/Assets/";
-        public Texture2D LoadTexture(string texturePath)
+        static public Texture2D LoadTexture(string texturePath)
         {
             Texture2D texture = new Texture2D(1, 1);
             ImageConversion.LoadImage(texture, File.ReadAllBytes(assetsPath + "/Textures/" + texturePath));
